9a84c245529694f9a933abe9059f0f748bd3814b
it.Baonk <bao.nguyenkhac@ivnd.com.vn>
2024-08-14 14:28:31 +0700

    init commit

---
+++ src/test/java/app/springboot30lesson/Springboot30lessonApplicationTests.java
@@ -0,0 +1,13 @@
+package app.springboot30lesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot30lessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
